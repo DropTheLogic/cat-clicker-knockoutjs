4146c9fc97d4516a245fa1e1adf9e364e1f63f0f
Daniel Marquez <DannyBrianMarquez@gmail.com>
2016-10-16 19:40:34 -0400

    feat: Add functionality to select between multiple cats
    * Normal array initialCats holds JSON-style cat object descriptors.
    
    * catList observable array has data pushed into it from initialCats.
    
    * Update cat images and names.
    
    * Minor CSS styling.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,55 @@
-var Cat = function() {
+var initialCats = [
+	{
+		name: 'Anton',
+		imgSrc: 'img/anton.png',
+		imgAttribution: '',
+		nicknames: ['Anderson Cooper', 'A-Rod', 'Rowdy, Roddy Anton']
+	},
+	{
+		name: 'Jeff',
+		imgSrc: 'img/jeff.png',
+		imgAttribution: '',
+		nicknames: ['Jarf', 'J Money', 'J Dilla']
+	},
+	{
+		name: 'Joey',
+		imgSrc: 'img/joey.png',
+		imgAttribution: '',
+		nicknames: ['Space Pimp', 'J Saxaphone', 'J Saxapahaw']
+	},
+	{
+		name: 'Maurice',
+		imgSrc: 'img/maurice.png',
+		imgAttribution: '',
+		nicknames: ['Mo Money', 'MoJo', 'Monday Morning']
+	},
+	{
+		name: 'Paul',
+		imgSrc: 'img/paul.png',
+		imgAttribution: '',
+		nicknames: ['Pretty Paul', 'Peepers Creepers', 'PB&J']
+	},
+	{
+		name: 'Randy',
+		imgSrc: 'img/randy.png',
+		imgAttribution: '',
+		nicknames: ['Remington Steel', 'Richie Rich', 'Macho Man Randy Savage']
+	},
+	{
+		name: 'Sarah',
+		imgSrc: 'img/sarah.png',
+		imgAttribution: '',
+		nicknames: ['Leg', 'Peg-leg', 'Princess Peach']
+	}
+];
+
+var Cat = function(data) {
+	var self = this;
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.attribution = ko.observable('');
-	this.nicknames = ko.observableArray([
-		'T-bone', 'T-money', 'T-Rex', 'Oedipus', 'Dr. Octagon'
-	]);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.attribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
 
 	this.title = ko.computed( function() {
 		if (this.clickCount() < 10) {
@@ -20,18 +64,28 @@ var Cat = function() {
 		else if (this.clickCount() < 60) {
 			return 'Suburbanite';
 		}
-		else if (this.clickCount() >= 80) {
+		else if (this.clickCount() >= 60) {
 			return 'Elder Statesman';
 		}
 	}, this);
 }
 
 var ViewModel = function() {
+	var self = this;
 
-	this.currentCat = ko.observable( new Cat() );
+	this.catList = ko.observableArray([]);
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
+
+	this.setCurrentCat = function(clickedCat) {
+		self.currentCat( clickedCat );
+	};
 
 	this.incrementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		this.clickCount(this.clickCount() + 1);
 	};
 }
 
